30b3a7539e014bd15de4ee85660140ff8a3acb4c
Ian <ssianfilipe@gmail.com>
2022-07-25 15:29:12 -0300

    setting packages up

---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,10 @@
+import Button from "../../components/Button";
+
+export default function Menu() {
+  return (
+    <main>
+      <nav className="menu"></nav>
+      <Button />
+    </main>
+  );
+}
