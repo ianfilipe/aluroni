c1da3b9d217e1609c0e4513ca06fb28b7b0e036f
Ian <ssianfilipe@gmail.com>
2022-07-26 13:08:30 -0300

    updating the types

--- src/pages/Menu/Items/Item/index.tsx
+++ src/pages/Menu/Items/Item/index.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import menu from "../items.json";
 import styles from "./Item.module.scss";
 
@@ -38,7 +39,17 @@ export default function Item(
           <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__type}>{category.label}</div>
+          <div
+            className={classNames({
+              [styles.item__type]: true,
+              [styles["item__type__pastas"]]: category.label === "Massas",
+              [styles["item__type__meats"]]: category.label === "Carnes",
+              [styles["item__type__combos"]]: category.label === "Combos",
+              [styles["item__type__vegans"]]: category.label === "Veganos",
+            })}
+          >
+            {category.label}
+          </div>
           <div className={styles.item__portion}>{size}g</div>
           <div className={styles.item__amountPeople}>
             Serve {serving} pessoa{serving === 1 ? "" : "s"}
