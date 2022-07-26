26c2f62e204e7e149206b7be1c9441ef2fbf0b28
Ian <ssianfilipe@gmail.com>
2022-07-26 19:05:13 -0300

    using react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import 'normalize.css'
-import Home from 'pages/Home'
-import Menu from 'pages/Menu'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
-const actualComponent = window.location.pathname === '/' ? <Home /> : <Menu />
+import Router from './routes'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
 	<React.StrictMode>
-		{actualComponent}
+		<Router />
 	</React.StrictMode>
 )
